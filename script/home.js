81cf73d6263c9ed1205178b3202480d902274e81
pllpezhman <pll.pezhman@gmail.com>
2023-03-20 23:58:16 +0330

    Creating branch v1.0 and completing the code on this branch and finally merging with the master branch

--- script/home.js
+++ script/home.js
@@ -1,19 +1,49 @@
 let elements = {
-    header:document.querySelector("header"),
-    mobileHamMenu: document.querySelector('.btnToggleMobileHamMenu'),
-    wrapper:document.querySelector('.wrapper'),
-    mobileMenu:document.querySelector(".mobile-menu"),
-}
+  header: document.querySelector("header"),
+  mobileHamMenuIcon: document.querySelector(".btnToggleMobileHamMenu"),
+  wrapper: document.querySelector(".wrapper"),
+  mobileMenu: document.querySelector(".mobile-menu"),
+  closeMobileMenu: document.getElementById("btnCloseMobileMenu"),
+  productsBTN: document.querySelector(".productsBTN"),
+  productSubMenu: document.querySelector(".productSubMenu"),
+};
 
+window.addEventListener("scroll", (e) => {
+  // console.log(e)
+  // console.log(getComputedStyle(elements.header).height)
+  // console.log((elements.header).scrollTop)
+  // console.log("p",window.scrollX)
+  // console.log(window.scrollY);
+  // console.log(scroll.pageXOffset);
 
-window.addEventListener('scroll', e =>{
-    if(window.scrollX > 10){
-        elements.header.classList.add("sticky");
-    }else{
-        elements.header.classList.remove("sticky");
-    }
-})
+  if (window.scrollY > 10) {
+    elements.header.classList.add("sticky");
+  } else {
+    elements.header.classList.remove("sticky");
+  }
+});
 
-mobileHamMenu.addEventListener("click", e => {
-    let e.
-})
+elements.mobileHamMenuIcon.addEventListener("click", (e) => {
+  if (window.scrollY < 10) {
+    elements.wrapper.style.trnasform = "scale(1)";
+  } else {
+    elements.wrapper.style.trnasform = "scale(0)";
+  }
+
+  elements.mobileMenu.classList.toggle("showMenuMobile");
+});
+
+elements.closeMobileMenu.addEventListener("click", () => {
+  elements.mobileMenu.classList.remove("showMenuMobile");
+  elements.wrapper.style.trnasform = "scale(0)";
+});
+
+elements.productsBTN.addEventListener("click", () => {
+  elements.productSubMenu.classList.toggle("showSubMenu");
+});
+
+document
+  .querySelector(".go-down>.icon-down-open")
+  .addEventListener("click", () => {
+    window.scrollTo({ top: 720, behavior: "smooth" });
+  });
